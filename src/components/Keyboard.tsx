8ba55eb26921faac21a49b4308c4442974017415
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-27 19:13:00 -0400

    config enter and delete buttons

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -67,11 +67,11 @@ export default function Keyboard({ guessedLetters, onTap, onEnter, onDelete }: K
                 <button
                     onMouseDown={onEnter}
                     className={
-                        'grey' === 'grey' ? 'grid place-items-center bg-gray-700 text-gray-50 w-9 h-9 p-1 rounded-sm'
-                            : 'grid place-items-center bg-gray-50 text-gray-700 w-9 h-9 p-1 rounded-sm'
+                        'grey' === 'grey' ? 'grid place-items-center bg-gray-700 text-gray-50 w-12 h-9 p-1 rounded-sm'
+                            : 'grid place-items-center bg-gray-50 text-gray-700 w-12 h-9 p-1 rounded-sm'
                     }
                 >
-                    <span>E</span>
+                    <span className='text-[.5rem] font-bold'>ACEPTAR</span>
                 </button>
                 <div className='grid grid-cols-7 auto-cols-max gap-1 md:gap-2 justify-center content-center'>
                     {lastRow.map((letter: string, i: number) => (
@@ -86,11 +86,17 @@ export default function Keyboard({ guessedLetters, onTap, onEnter, onDelete }: K
                 <button
                     onMouseDown={onDelete}
                     className={
-                        'grey' === 'grey' ? 'grid place-items-center bg-gray-700 text-gray-50 w-9 h-9 p-1 rounded-sm'
-                            : 'grid place-items-center bg-gray-50 text-gray-700 w-9 h-9 p-1 rounded-sm'
+                        'grey' === 'grey' ? 'grid place-items-center bg-gray-700 text-gray-50 w-12 h-9 p-1 rounded-sm'
+                            : 'grid place-items-center bg-gray-50 text-gray-700 w-12 h-9 p-1 rounded-sm'
                     }
                 >
-                    <span>D</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" viewBox="0 0 512 512">
+                        <rect width="512" height="512" fill="none" />
+                        <path fill="currentColor"
+                            d="M227.313 363.313L312 278.627l84.687 84.686l22.626-22.626L334.627 256l84.686-84.687l-22.626-22.626L312 233.373l-84.687-84.686l-22.626 22.626L289.373 256l-84.686 84.687z" />
+                        <path fill="currentColor"
+                            d="M472 64H194.644a24.1 24.1 0 0 0-17.42 7.492L16 241.623v28.754l161.224 170.131a24.1 24.1 0 0 0 17.42 7.492H472a24.03 24.03 0 0 0 24-24V88a24.03 24.03 0 0 0-24-24m-8 352H198.084L48 257.623v-3.246L198.084 96H464Z" />
+                    </svg>
                 </button>
             </div>
         </section>
