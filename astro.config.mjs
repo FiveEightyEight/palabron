66d441e3e0b0be490202ef5ef7c2ed2503f69482
FiveEightyEight <robert.abreu3@gmail.com>
2024-07-28 12:27:10 -0400

    config astro for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()]
+  site: 'https://palabron.app',
+  base: '',
+  integrations: [tailwind(), react(), icon(), compress()]
 });
