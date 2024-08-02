b5a050c62e71bce074ce6a4557646dc3fbd14664
FiveEightyEight <robert.abreu3@gmail.com>
2024-08-02 16:48:28 -0400

    show instruction modal when game boots

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -23,6 +23,7 @@ export default function Game() {
     const [currentPosition, setCurrentPosition] = React.useState<number>(0);
     const [deleteAtCurrentPosition, setDeleteAtCurrentPosition] = React.useState<boolean>(false);
     const [lastLetterSet, setLastLetterSet] = React.useState<boolean>(false);
+    const [showInstructions, setShowInstructions] = React.useState<boolean>(false);
 
     const modalRef = React.useRef<HTMLElement>(null)
     const guessContainerRef = React.useRef<HTMLDivElement>(null);
@@ -32,6 +33,15 @@ export default function Game() {
         const tl = gsap.timeline({ paused: true });
         setTimeline(tl);
         const result: null | GameState = JSON.parse(localStorage.getItem(today) || 'null');
+        try {
+            const instructionsSeen: boolean = JSON.parse(localStorage.getItem('instructions') || 'null');
+            if (!instructionsSeen) {
+                setShowInstructions(true);
+            }
+        } catch (err) {
+            console.log('err', err)
+        }
+        
         if (result) {
             /*
                 "2024-07-26": {
@@ -316,12 +326,16 @@ export default function Game() {
         <>
             <div className='flex flex-col justify-between pt-1 pb-2 md:py-5 h-[100dvh] w-[100dvw] bg-black'>
                 <section className='flex justify-between text-center border-b-2 border-opacity-30 border-slate-400 pb-2 '>
-                    <div className='w-10 h-10'/>
+                    <div className='w-10 h-10' />
                     <div className='-mb-2 '>
                         <h1 className='text-gray-50 text-center text-xl font-bold'>PALABRÓN</h1>
                         <span className='-mt-5 text-gray-300 text-center text-[.6rem] italic'>({packageJSON.version})</span>
                     </div>
-                    <button aria-label='Instrucciones' className='grid place-content-center text-white w-10 h-10'>
+                    <button
+                        aria-label='Instrucciones'
+                        className='grid place-content-center text-white w-10 h-10'
+                        onMouseDown={() => setShowInstructions(!showInstructions)}
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"><rect width="24" height="24" fill="none" /><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2m0 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16m0 12a1 1 0 1 1 0 2a1 1 0 0 1 0-2m0-9.5a3.625 3.625 0 0 1 1.348 6.99a.8.8 0 0 0-.305.201c-.044.05-.051.114-.05.18L13 14a1 1 0 0 1-1.993.117L11 14v-.25c0-1.153.93-1.845 1.604-2.116a1.626 1.626 0 1 0-2.229-1.509a1 1 0 1 1-2 0A3.625 3.625 0 0 1 12 6.5" /></g></svg>
                     </button>
                 </section>
@@ -368,7 +382,28 @@ export default function Game() {
                         </div>
                     </Modal>
                 )}
-            </div>\
+                {showInstructions && (
+                    <Modal ref={modalRef}>
+                        <div className="bg-black bg-t border-4 bg-opacity-85 border-opacity-50 border-slate-500 p-4 rounded-2xl h-auto w-auto place-content-center">
+                            <h3 className="text-gray-50 text-center text-2xl font-bold pt-4">Como Jugar</h3>
+                            <div>
+                                <p>
+                                    Como jugar...
+                                </p>
+                            </div>
+                            <div className="flex justify-center p-4">
+                                <button
+                                    className='rounded-xl w-36 h-14 border-2 border-gray-400 bg-slate-700 active:bg-slate-900 focus:bg-slate-900 text-gray-50 text-nowrap font-extrabold'
+                                    aria-label='Juega de nuevo'
+                                    onMouseDown={() => setShowInstructions(!showInstructions)}
+                                >
+                                    Listo!
+                                </button>
+                            </div>
+                        </div>
+                    </Modal>
+                )}
+            </div>
         </>
     )
 }
